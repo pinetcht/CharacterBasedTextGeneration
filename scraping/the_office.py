9eecc13ea7ef1a2b501966e03644943097c249b5
gaeverts1 <gaeverts1@gmail.com>
2024-11-19 16:20:11 -0800

    fixing up repo organization

--- scraping/the_office.py
+++ scraping/the_office.py
@@ -1,21 +1,21 @@
 import json
 
-with open('the-office.json', 'r') as file:
+with open('./data/the-office.json', 'r') as file:
     data = json.load(file)
 
 def extract_michael_lines(data):
-    michael_lines = []
+    lines = []
     if isinstance(data, list):
         for item in data:
-            michael_lines.extend(extract_michael_lines(item))
+            lines.extend(extract_michael_lines(item))
     elif isinstance(data, dict):
         if data.get("character") == "Michael":
-            michael_lines.append(data["line"])
+            lines.append(data["line"])
         for key, value in data.items():
-            michael_lines.extend(extract_michael_lines(value))
-    return michael_lines
+            lines.extend(extract_michael_lines(value))
+    return lines
 
-michael_lines = extract_michael_lines(data)
+lines = extract_michael_lines(data)
 
-with open('michael_lines.txt', 'w') as output_file:
-    output_file.write("\n".join(michael_lines))
+with open('michael_dialogue.txt', 'w') as output_file:
+    output_file.write("\n".join(lines))
