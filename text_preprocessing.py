d59ad9e7de2a28d8834eb86f57ad0284703279e5
gaeverts1 <gaeverts1@gmail.com>
2024-11-26 13:54:03 -0800

    fixed phoebe yay

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -22,9 +22,10 @@ import string
 with open("./data/phoebe_buffay_dialogue.txt") as file:
     with open("./data/preprocessed/phoebe_preprocessed.txt", "w") as processed_file:
         for line in file:
-            line = line.lower()
-            p = re.compile("([" + re.escape(string.punctuation) + "])")
-            line = re.sub(p, " \\1 ", line)
-            line = re.sub("\s+", " ", line)
-            processed_file.write(line)
+            if not re.search('^$', line):
+                line = line.lower()
+                p = re.compile("([" + re.escape(string.punctuation) + "])")
+                line = re.sub(p, " \\1 ", line)
+                line = re.sub("\s+", " ", line)
+                processed_file.write(line + "\n")
                 
